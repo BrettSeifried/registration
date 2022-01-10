878c3e9a3fa2f9d4b7ef8e01236313e59a832aa1
Brett Seifried <seifriedbrett@gmail.com>
2022-01-10 12:07:42 -0800

    npm i and routing set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <h1> testing page </h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <p>home page</p>
+          </Route>
+          <Route path="/auth">
+            <p> auth page</p>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </main>
   );
 }
 
